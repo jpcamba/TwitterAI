2af971b84d4a9568df2c5f0bc69d080a1e340de3
jpcamba <camba.jannieca@gmail.com>
2015-05-30 14:32:03 +0800

    Updated TwitterAI.java
    Pre-final gui

--- TwitterAI.java
+++ TwitterAI.java
@@ -1,9 +1,9 @@
 import java.util.*;
 import java.math.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.text.DefaultCaret;
 
 class TwitterAI{
 
@@ -16,11 +16,15 @@ class TwitterAI{
 		TwitterAI t = new TwitterAI();
 	}
 
-	private JFrame frame;
-	private JTextArea typebox;
-	private JTextArea tweetbox;
-	private JButton check;
-
+	private JFrame frame = new JFrame("Sinong nag-tweet?");	
+	private JTextArea tweetbox = new JTextArea();
+	private JScrollPane scroll, scroll2;	
+	private JTextArea typebox = new JTextArea();
+	private JButton check = new JButton();		
+	private JPanel tweetcon = new JPanel();
+	private JPanel typecon = new JPanel();
+	private JPanel checkcon = new JPanel();
+	
 	private static double lambda;
 	private ArrayList<Category> classes;
 	private ArrayList<String> vocab;
@@ -44,56 +48,47 @@ class TwitterAI{
 		classes = handler.readModel("tempmodel");
 	}
 
-	private JScrollPane scroll;
-
 	public void initializeGUI(){
-		frame = new JFrame("Sinong nag-tweet?");
-
-		frame.setLayout(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(622, 400);
-		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
-
-		typebox = new JTextArea(4, 34);
-		// JTextArea chatbox = new JTextArea(2, 33);
-		// chatbox.append("no tweets to show");
-
-		tweetbox = new JTextArea(18, 33);
-		tweetbox.setLocation(10, 10);
-		tweetbox.setVisible(true);
-		tweetbox.setEditable(false);
-    DefaultCaret caret = (DefaultCaret)tweetbox.getCaret();
-    caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-
-
-		scroll = new JScrollPane(tweetbox);
-		scroll.setSize(390, 250);
-    scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-    scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-
-		JPanel panel = new JPanel();
-		JPanel right = new JPanel();
-
-		check = new JButton("Check");
-		check.setPreferredSize(new Dimension(180, 45));
-		check.addActionListener(new ButtonListener());
-
-		panel.setSize(390, 350);
-		panel.setLocation(10, 10);
-		panel.add(scroll);
-		panel.add(typebox);
-		panel.setVisible(true);
-
-
-		right.setSize(230, 350);
-		right.setLocation(390, 10);
-		right.add(check);
-		right.setVisible(true);
-
-		frame.add(panel);
-		frame.add(right);
 		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 550);
+		frame.setLayout(new FlowLayout());      
+		frame.setResizable(false);
+
+		Border border = BorderFactory.createLineBorder(Color.black, 1);
+		
+		tweetbox.setOpaque(true);
+		tweetbox.setEditable(false);
+		tweetbox.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		scroll = new JScrollPane(tweetbox);
+		scroll.setPreferredSize(new Dimension(730, 350));
+		scroll.setBorder(border);
+		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+		typebox.setOpaque(true);
+		typebox.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		scroll2 = new JScrollPane(typebox);
+		scroll2.setBorder(border);
+		scroll2.setPreferredSize(new Dimension(730, 70));
+		scroll2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+		scroll2.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		
+		check.setText("Check");
+		check.setSize(50, 55);
+		check.addActionListener(new ButtonListener());
+		
+		tweetcon.add(scroll);		
+		typecon.add(scroll2);		
+		checkcon.add(check);		
+		
+		tweetcon.setPreferredSize(new Dimension(750, 370));
+		typecon.setPreferredSize(new Dimension(750, 80));
+		checkcon.setPreferredSize(new Dimension(750, 60));
+		
+		frame.add(tweetcon);
+		frame.add(typecon);
+		frame.add(checkcon);
 	}
 
 	public void checkTweet(String tweet){
